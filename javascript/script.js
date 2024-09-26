b7aa1b6458ddfbbcda960001c43a9dec177f7a48
Monika-Choudhary <monikachahota@gmail.com>
2024-09-26 15:50:30 +0200

    fixed header lyout in index.html, added scroll effect to header of main page

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
     const header = document.getElementById("header");
-    const contentTop = document.querySelector('.content').offsetTop;
+    const contentTop = document.querySelector('.main').offsetTop;
   
     if (window.scrollY >= contentTop) {
       header.style.top = "-100px"; // Move header out of view
