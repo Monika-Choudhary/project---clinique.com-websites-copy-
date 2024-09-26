cf8d1a8831b5d6ef378a6a5005e35f52cee21e5d
Monika-Choudhary <monikachahota@gmail.com>
2024-09-26 14:56:20 +0200

    feat: style to header with scroll effect, basiy style for main content

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function () {
+    const header = document.getElementById("header");
+    const contentTop = document.querySelector('.content').offsetTop;
+  
+    if (window.scrollY >= contentTop) {
+      header.style.top = "-100px"; // Move header out of view
+    } else {
+      header.style.top = "0"; // Reset to sticky
+    }
+  });
+  
