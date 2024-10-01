3aa1649d458cf6a3f2f46991e2184730573fb80d
Monika-Choudhary <monikachahota@gmail.com>
2024-10-01 11:28:42 +0200

    deleted: class main

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
     const header = document.getElementById("header");
-    const contentTop = document.querySelector('.main').offsetTop;
+    const contentTop = document.querySelector('main').offsetTop;
   
     if (window.scrollY >= contentTop) {
       header.style.top = "-100px"; // Move header out of view
